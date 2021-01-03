3de904f49f98db6d23d472aee894aed7305eae7d
Alexey Shmalko <rasen.dubi@gmail.com>
2021-01-03 20:19:53 +0200

    chore: add flake.nix

---
+++ flake.nix
@@ -0,0 +1,13 @@
+{
+  description = "Arc diagrams";
+
+  outputs = { self, nixpkgs }: {
+    devShell.x86_64-linux =
+      let pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      in pkgs.mkShell {
+        nativeBuildInputs = [
+          pkgs.nodejs-14_x
+        ];
+      };
+  };
+}
