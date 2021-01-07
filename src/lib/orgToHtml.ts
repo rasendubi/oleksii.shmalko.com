a692a8d46edd2a9fd7ce2348e942990ac9a1ab0f
Alexey Shmalko <rasen.dubi@gmail.com>
2021-01-07 03:50:23 +0200

    feat: demote headings so that note title is the only h1 tag

--- src/lib/orgToHtml.ts
+++ src/lib/orgToHtml.ts
@@ -4,6 +4,7 @@ import urls from 'rehype-urls';
 import raw from 'rehype-raw';
 import vfile from 'vfile';
 import { select } from 'unist-util-select';
+import visit from 'unist-util-visit';
 
 import orgParse from '@/org/unified-org-parse';
 import org2rehype from '@/org/unified-org-rehype';
@@ -17,6 +18,7 @@ const processor = unified()
   .use(org2rehype)
   .use(raw)
   .use(urls, processUrls)
+  .use(demoteHeadings)
   .use(html);
 
 export default async function orgToHtml(path: string, org: string) {
@@ -68,6 +70,36 @@ function removeCards() {
         child.type === 'headline' &&
         child.title[0]?.value.toLowerCase() === 'cards'
       );
+    });
+  }
+}
+
+function demoteHeadings() {
+  return transformer;
+
+  function transformer(tree: any) {
+    visit(tree, (node: any) => {
+      if (node.type !== 'element') return;
+      switch (node.tagName) {
+        case 'h1':
+          node.tagName = 'h2';
+          break;
+        case 'h2':
+          node.tagName = 'h3';
+          break;
+        case 'h3':
+          node.tagName = 'h4';
+          break;
+        case 'h4':
+          node.tagName = 'h5';
+          break;
+        case 'h5':
+          node.tagName = 'h6';
+          break;
+        case 'h6':
+          node.tagName = 'p';
+          break;
+      }
     });
   }
 }
