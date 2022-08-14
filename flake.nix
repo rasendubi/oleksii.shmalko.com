df7efc837590bde6af0d886eab70004ca2558080
Alexey Shmalko <rasen.dubi@gmail.com>
2022-08-15 01:59:00 +0300

    chore: update node to 16

--- flake.nix
+++ flake.nix
@@ -1,14 +1,19 @@
 {
   description = "www.alexeyshmalko.com";
 
-  outputs = { self, nixpkgs }: {
-    devShell.x86_64-linux =
-      let pkgs = nixpkgs.legacyPackages.x86_64-linux;
-      in pkgs.mkShell {
-        nativeBuildInputs = [
-          pkgs.nodejs-14_x
-          pkgs.awscli2
-        ];
-      };
-  };
-}
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = [
+            pkgs.nodejs-16_x
+            pkgs.awscli2
+          ];
+        };
+      });
+  }
