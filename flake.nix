1501da14f9d5afd1af93134c337dfd808b6239d4
Oleksii Shmalko <rasen.dubi@gmail.com>
2025-11-13 22:59:37 +0200

    [flake] upgrade

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "oleksii.shmalko.com";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils }:
@@ -11,9 +11,8 @@
       in {
         devShell = pkgs.mkShell {
           nativeBuildInputs = [
-            pkgs.nodejs_22
-            pkgs.nodejs_22.pkgs.pnpm
-            # pkgs.nodejs-16_x.pkgs.pnpm
+            pkgs.nodejs_24
+            pkgs.nodejs_24.pkgs.pnpm
 
             pkgs.python3
           ];
